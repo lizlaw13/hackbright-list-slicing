cac155c9abf7227b1fc474ad5c0b6a6cc6fbf05a
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-10 15:16:49 -0800

    Custom Length and Custom Append Function

--- further_study.py
+++ further_study.py
@@ -31,7 +31,12 @@ def custom_len(input_list):
 
     """
 
-    return 0
+    count = 0
+
+    for item in input_list:
+        count += 1
+
+    return count
 
 
 # For the next four exercises, you'll need to be clever and think about ways
@@ -59,7 +64,7 @@ def custom_append(input_list, value):
 
     """
 
-    pass
+    input_list[custom_len(input_list) + 1:] = [value]
 
 
 def custom_extend(input_list, second_list):
